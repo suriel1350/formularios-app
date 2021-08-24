bcf7f8377b40871e482b6f764b84003286684be4
suriel_asael <kato_1350@hotmail.com>
2021-08-23 23:28:45 -0500

    last: last commit

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { nombreApellidoPattern, emailPattern, noPuedeSerStrider } from '../../../shared/validator/validaciones';
 import { ValidatorService } from '../../../shared/validator/validator.service';
-import { EmailValidatorService } from "../../../shared/validator/email-validator.service";
+//import { EmailValidatorService } from "../../../shared/validator/email-validator.service";
 
 @Component({
   selector: 'app-registro',
@@ -15,7 +15,7 @@ export class RegistroComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [ Validators.required, Validators.pattern( this.validatorService.nombreApellidoPattern ) ] ],
-    email: ['', [ Validators.required, Validators.pattern( this.validatorService.emailPattern ) ], [ this.emailValidator ] ],
+    email: ['', [ Validators.required, Validators.pattern( this.validatorService.emailPattern ) ] ],
     username: ['', [ Validators.required, this.validatorService.noPuedeSerStrider ] ],
     password: ['', [ Validators.required, Validators.minLength(6) ]  ],
     password2: ['', [ Validators.required ]  ],
@@ -40,7 +40,7 @@ export class RegistroComponent implements OnInit {
 
   constructor( private fb: FormBuilder,
                private validatorService: ValidatorService,
-               private emailValidator: EmailValidatorService ) { }
+                ) { }
 
   ngOnInit(): void {
 
